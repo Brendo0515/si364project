5fad730a12bc27fdd79d35dbe226aa480b82bab8
Brendo0515 <46972987+Brendo0515@users.noreply.github.com>
2020-04-09 22:31:50 -0400

    Create models.py

---
+++ models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.core.validators import MinLengthValidator
+from django.contrib.auth.models import User
+from django.conf import settings
+
+class Song(models.Model) :
+    title = models.CharField(
+            max_length=200
+    )
+    album = models.TextField()
+    artist = models.TextField()
+    year = models.IntegerField(max_digits=4, null=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    #comments
+    comments = models.ManyToManyField(settings.AUTH_USER_MODEL,
+        through='Comment', related_name='comments_owned')
+
+    # picture
+    picture = models.BinaryField(null=True, editable=True)
+    content_type = models.CharField(max_length=256, null=True, help_text='The MIMEType of the file')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Shows up in the admin list
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model) :
+    text = models.TextField(
+        validators=[MinLengthValidator(3, "Comment must be greater than 3 characters")]
+    )
+
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Shows up in the admin list
+    def __str__(self):
+        if len(self.text) < 15 : return self.text
+        return self.text[:11] + ' ...'
